c9529c32cf5109902b480670ecd6920df9b1ff6b
vladimirtarasov <vptarasov1998@gmail.com>
2024-01-21 08:49:09 +0200

    Implement object detector with tensor flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin") version "2.7.6"
+    id("de.undercouch.download")
 }
 
 android {
@@ -36,12 +38,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
+    buildFeatures {
+        viewBinding = true
     }
 }
+
+extra["ASSET_DIR"] = "$projectDir/src/main/assets"
+extra["TEST_ASSETS_DIR"] = "$projectDir/src/androidTest/assets"
+apply(from = "download_models.gradle")
 
 dependencies {
 
@@ -52,16 +56,18 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     
     //CameraX
+    implementation("androidx.camera:camera-core:1.3.1")
     implementation("androidx.camera:camera-camera2:1.3.1")
     implementation("androidx.camera:camera-lifecycle:1.3.1")
     implementation("androidx.camera:camera-view:1.3.1")
     
-    //MLKit
-    implementation("com.google.mlkit:vision-common:17.3.0")
-    implementation("com.google.mlkit:camera:16.0.0-beta3")
-    implementation("com.google.mlkit:object-detection:17.0.0")
-    implementation("com.google.mlkit:object-detection-custom:17.0.1")
-    implementation("com.google.mlkit:image-labeling:17.0.7")
-    implementation("com.google.mlkit:image-labeling-custom:17.0.2")
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
+    //Tensorflow
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
 
 }
--- app/src/main/java/com/vnteam/objectdetector/MainActivity.kt
+++ app/src/main/java/com/vnteam/objectdetector/MainActivity.kt
@@ -1,127 +1,25 @@
 package com.vnteam.objectdetector
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
-import android.util.Size
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.view.PreviewView
-import androidx.core.app.ActivityCompat
-import com.google.mlkit.common.model.LocalModel
-import com.google.mlkit.vision.camera.CameraSourceConfig
-import com.google.mlkit.vision.camera.CameraXSource
-import com.google.mlkit.vision.camera.DetectionTaskCallback
-import com.google.mlkit.vision.objects.DetectedObject
-import com.google.mlkit.vision.objects.ObjectDetection
-import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
+import androidx.appcompat.app.AppCompatActivity
+import com.vnteam.objectdetector.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private var previewView: PreviewView? = null
-    private var graphicOverlay: GraphicOverlay? = null
-    private var needUpdateGraphicOverlayImageSourceInfo = false
-    private var lensFacing: Int = CameraSourceConfig.CAMERA_FACING_BACK
-    private var cameraXSource: CameraXSource? = null
-    private var customObjectDetectorOptions: CustomObjectDetectorOptions? = null
-    private var targetResolution: Size? = null
-
-    private val isPortraitMode: Boolean
-        get() = applicationContext.resources.configuration.orientation != Configuration.ORIENTATION_LANDSCAPE
-    private val localModel: LocalModel
-        get() = LocalModel.Builder().setAssetFilePath("custom_models/bike_car_model.tflite").build()
-
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { isGranted: Map<String, @JvmSuppressWildcards Boolean>? ->
-            if (isGranted?.values?.contains(false) == true) {
-                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
-            } else {
-                startCameraXSource()
-            }
-        }
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        previewView = findViewById(R.id.preview_view)
-        graphicOverlay = findViewById(R.id.graphic_overlay)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (cameraXSource != null &&
-            PreferenceUtils.getCustomObjectDetectorOptionsForLivePreview(this, localModel).equals(customObjectDetectorOptions) &&
-            PreferenceUtils.getCameraXTargetResolution(applicationContext, lensFacing) != null &&
-            PreferenceUtils.getCameraXTargetResolution(applicationContext, lensFacing) == targetResolution) {
-            startCameraXSource()
+    override fun onBackPressed() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            finishAfterTransition()
         } else {
-            createThenStartCameraXSource()
+            super.onBackPressed()
         }
-    }
-
-    private fun startCameraXSource() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            cameraXSource?.start()
-        } else {
-            requestPermissionLauncher.launch(arrayOf(Manifest.permission.CAMERA))
-        }
-    }
-
-    private fun createThenStartCameraXSource() {
-        cameraXSource?.close()
-        customObjectDetectorOptions =
-            PreferenceUtils.getCustomObjectDetectorOptionsForLivePreview(applicationContext, localModel)
-        val objectDetector = customObjectDetectorOptions?.let { ObjectDetection.getClient(it) }
-        val detectionTaskCallback: DetectionTaskCallback<List<DetectedObject>> =
-            DetectionTaskCallback<List<DetectedObject>> { detectionTask ->
-                detectionTask
-                    .addOnSuccessListener { results -> onDetectionTaskSuccess(results) }
-                    .addOnFailureListener { e -> onDetectionTaskFailure(e) }
-            }
-
-        targetResolution =
-            PreferenceUtils.getCameraXTargetResolution(applicationContext, lensFacing)
-        objectDetector?.let { CameraSourceConfig.Builder(applicationContext, it, detectionTaskCallback).setFacing(lensFacing) }?.apply {
-            targetResolution?.let { setRequestedPreviewSize(it.width, it.height) }
-            cameraXSource = previewView?.let { CameraXSource(build(), it) }
-        }
-        needUpdateGraphicOverlayImageSourceInfo = true
-        startCameraXSource()
-    }
-
-    private fun onDetectionTaskSuccess(results: List<DetectedObject>) {
-        graphicOverlay?.apply {
-            clear()
-            if (needUpdateGraphicOverlayImageSourceInfo) {
-                cameraXSource?.previewSize.takeIf { it != null }?.let { size ->
-                    val isImageFlipped =
-                        cameraXSource?.cameraFacing == CameraSourceConfig.CAMERA_FACING_FRONT
-                    if (isPortraitMode) {
-                        setImageSourceInfo(size.height, size.width, isImageFlipped)
-                    } else {
-                        setImageSourceInfo(size.width, size.height, isImageFlipped)
-                    }
-                    needUpdateGraphicOverlayImageSourceInfo = false
-                }
-            }
-            results.forEach { detectedObject ->
-                add(ObjectGraphic(this, detectedObject))
-            }
-            add(InferenceInfoGraphic(this))
-            postInvalidate()
-        }
-    }
-
-    private fun onDetectionTaskFailure(e: Exception) {
-        graphicOverlay?.clear()
-        graphicOverlay?.postInvalidate()
-        val error = "Failed to process. Error: " + e.localizedMessage
-        Toast.makeText(graphicOverlay?.context, error, Toast.LENGTH_SHORT).show()
     }
 }
